fa70fbca9b33f76f5b823366a8c3bedc44ab0afc
Federico N <nfederico@live.com>
2021-03-24 12:24:10 -0300

    Desafio03

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const curso ='Coder House'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+          <h1>Aprendiendo React con {curso} - Escribiendo JSX</h1>
+          <nav>
+            <ul>
+              <li><a href="#">Inicio</a></li>              
+              <li><a href="#">Productos</a></li>
+              <li><a href="#">Carrito</a></li>                                       
+            </ul>
+          </nav>
+          
+
     </div>
   );
 }
