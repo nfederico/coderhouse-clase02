1cb4c4c678d75336de888e61e0c4a5e29fccd20d
Federico N <nfederico@live.com>
2021-03-25 20:08:16 -0300

    Subiendo modificacione, agrego /COMPONENTS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 
 import './App.css';
+import NavBar from  './components/NavBar';
 
 function App() {
   const curso ='Coder House'
   return (
-    <div className="App">
-     
-          <h1>Aprendiendo React con {curso} - Escribiendo JSX</h1>
-          <nav>
-            <ul>
-              <li><a href="#">Inicio</a></li>              
-              <li><a href="#">Productos</a></li>
-              <li><a href="#">Carrito</a></li>                                       
-            </ul>
-          </nav>
-          
+    <>     
+          <h1>Americana Design </h1>          
+          <NavBar/>
 
-    </div>
+    </>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+
+const NavBar = () => 
+{return (
+    <nav>
+        <ul>
+        <li><a href="#">Inicio</a></li>              
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Carrito</a></li>                                       
+        </ul>
+    </nav>
+)
+}
+
+export default NavBar
