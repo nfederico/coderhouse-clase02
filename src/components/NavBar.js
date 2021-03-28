a85fd01ab49661c4ce2807ca94cd105350617fca
Federico N <nfederico@live.com>
2021-03-28 20:40:56 -0300

    Agrego Brand para reentrega

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,23 @@
+import companyLogo from '../resources/logoAmericana.png';
 
 const NavBar = () => 
 {
 return (
+    
     <nav>
-        <ul>
+        <ul>        
+        <li>
+            <img
+                alt=""
+                src= {companyLogo}
+                width="30"
+                height="35"
+                className="logo"
+            />
+            <a href="#">Americana</a>
+        </li>              
         <li><a href="#">Inicio</a></li>              
-        <li><a href="#">Productos</a></li>
+        <li><a href="#">Productos</a></li>            
         <li><a href="#">Carrito</a></li>                                       
         </ul>
     </nav>
